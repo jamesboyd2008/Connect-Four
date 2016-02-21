01a7a3f748968a4a4e349773c4e48d4719fc459e
Reuben Brandt <email": null>
2016-02-20 17:33:04 -0800

    Full MVP functionality

--- frontEnd.js
+++ frontEnd.js
@@ -1,14 +1,56 @@
 $(document).ready(function(){
+  $(".column").hover(function(e){
+    $('.hover').remove()
+    var columnId = $(this).attr('id');
+    var column = $('#' + columnId).children();
+    if (turnCounter % 2 === 0){
+      $(column[0]).html('<div class="token1 hover"></div>')}
+    else
+      $(column[0]).html('<div class="token2 hover"></div>')
+
+  });
   $(".column").on("click", function(e){
     // console.log(e)
-    var columnId = $(this).attr('id')
-    console.log(columnId)
+    $('.hover').remove()
+    var columnId = $(this).attr('id');
 
-    console.log(str)
-    $("#" +columnId + ' .square .empty:last-child').append("<div class='token1'></div>");
-    // $('.token1').css({bottom: 5, position:'absolute'});
+    var column = $('#' + columnId).children();
+
+    var lastEmpty = ifFull(column);
+
+    if (lastEmpty != undefined)
+      appendToken(lastEmpty);
   });
-
-
-
 });
+
+turnCounter = 0
+
+  function findEmpty(column){
+    for(i=0; i< column.length; i++){
+      if ($(column[i]).attr('class') === "square full"){
+        return column[i-1]
+      }
+    }
+    return $(column).last()
+  }
+
+  function appendToken(lastEmpty){
+    if (turnCounter % 2 === 0)
+    $(lastEmpty).append('<div class="token1"></div>');
+  else
+    $(lastEmpty).append('<div class="token2"></div>');
+
+    $(lastEmpty).addClass('full');
+    turnCounter += 1
+  }
+
+  function ifFull(column){
+    if($(column[1]).attr("class")=== "square full")
+      alert("Column already full");
+    else
+      return findEmpty(column)
+  }
+
+
+
+
